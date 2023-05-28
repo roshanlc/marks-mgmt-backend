03542de3bb73845f810f31272baeaa172af1bf6b
Roshan Lamichhane <lamichhanersn@gmail.com>
2023-05-28 07:30:42 +0545

    Basic endpoint to test on hosting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Hello")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const helmet = require("helmet")
+
+// Add cors support
+app.use(
+  cors({
+    preflightContinue: true,
+    origin: "*",
+  })
+)
+
+// Helmet prevents from sending response headers with explicit info
+app.use(helmet())
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "You are here at the altar of the great Jedis!!!" })
+  return
+})
+app.listen(9000, () => {
+  console.log("-".repeat(75))
+  console.log("⚡Started at 9000 : ", new Date().toLocaleString())
+})
