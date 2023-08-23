e1c58a024831cc0ee5e059c1af4b0c34679752ae
Roshan Lamichhane <lamichhanersn@gmail.com>
2023-08-23 14:50:54 +0545

    Added endpoint to update marks detail by admin

--- src/routes/admin/student-marks.js
+++ src/routes/admin/student-marks.js
@@ -9,6 +9,10 @@ const {
   getStudentMarks,
   getStudentMarksBySemester,
 } = require("../../db/students/student-marks")
+const Joi = require("joi")
+const { escapeColon } = require("../../helper/utils")
+const { updateMarksOfStudent } = require("../../db/teachers/teacher-courses")
+const { getAStudentDetails } = require("../../db/students/students")
 
 // list all students' marks
 router.get("/", async function (req, res) {
@@ -74,6 +78,51 @@ router.get("/:student_id", async function (req, res) {
   }
 
   res.status(200).json(marks.result)
+})
+
+const addMarksSchema = Joi.object({
+  studentId: Joi.number().min(0).required(),
+  theory: Joi.number().min(0).max(100).required(),
+  practical: Joi.number().min(0).max(100).required(),
+  courseId: Joi.number().min(0).required(),
+  notQualified: Joi.boolean().default(false).optional(),
+  absent: Joi.boolean().default(false).optional(),
+  expelled: Joi.boolean().default(false).optional(),
+})
+
+// update marks of a student for a course
+router.put("/", async function (req, res) {
+  const err = addMarksSchema.validate(req.body).error
+  // incase of errors during schema validation
+  if (err !== undefined && err !== null) {
+    res.status(400).json(errorResponse("Bad Request", escapeColon(err.message)))
+    return
+  }
+
+  // request body json
+  const details = req.body
+
+  // Now update the marks
+  const marksAddition = await updateMarksOfStudent(
+    details.studentId,
+    details.courseId,
+    details.theory,
+    details.practical,
+    details.notQualified,
+    details.absent,
+    details.expelled
+  )
+
+  if (marksAddition.err !== null && marksAddition.err !== undefined) {
+    res
+      .status(responseStatusCode.get(marksAddition.err.error.title) || 400)
+      .json(marksAddition.err)
+    return
+  }
+
+  // successfully created resource
+  res.status(200).json(marksAddition.result)
+  return
 })
 
 module.exports = router
